a4e425198dad49e275787b9fdd4e7390767a1a43
Vilachao <josemaria1986@gmail.com>
2014-02-18 13:55:21 +0100

    subo clase telefono

---
+++ SAD/SAD/telefono/NATelefonosViewController.h
@@ -0,0 +1,17 @@
+//
+//  NATelefonosViewController.h
+//  SaDmenu
+//
+//  Created by Jose Maria on 18/02/14.
+//  Copyright (c) 2014 Kometasoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NATelefonosViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
+
+
+@end
